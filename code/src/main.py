c66f24ccc16177e830ee7bf32f8e1416c14d5cd6
Jiri Spilka <jirka.spilka@gmail.com>
2024-05-13 16:55:58 +0200

    fix

--- code/src/main.py
+++ code/src/main.py
@@ -1,92 +1,50 @@
 import os
 
 from apify import Actor
-from langchain.docstore.document import Document
-from langchain_community.document_loaders import ApifyDatasetLoader
-from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from .vcs import TypeDb, get_database
-from .models.chroma_input_model import ChromaIntegration  # type: ignore
-from .models.pinecone_input_model import PineconeIntegration  # type: ignore
+from .constants import SupportedVectorStoresEn
+from .core import main as main_f
+from .models.chroma_input_model import ChromaIntegration
+from .models.pinecone_input_model import PineconeIntegration
 
-
-def get_nested_value(data: dict, keys: str) -> str:
-    """
-    Extract nested value from dict.
-
-    Example:
-      >>> get_nested_value({"a": "v1", "c1": {"c2": "v2"}}, "c1.c2")
-      'v2'
-    """
-
-    keys_list = keys.split(".")
-    result = data
-
-    for key in keys_list:
-        if key in result:
-            result = result[key]
-        else:
-            # If any of the keys are not found, return None
-            return ""
-
-    return result  # type: ignore
+print("Imported entrypoint.py")
 
 
 async def main():
     async with Actor:
 
+        Actor.log.info("Starting the Actor")
+        print("Starting the Actor")
+
         if not (actor_input := await Actor.get_input() or {}):
             await Actor.fail(status_message="No input provided", exit_code=1)
 
-        Actor.log.debug("Received start argument: %s", actor_input)
+        print("Actor path in the docker context: ", os.getenv("ACTOR_PATH_IN_DOCKER_CONTEXT"))
 
-        # os.environ["OPENAI_API_KEY"] = actor_input.openai_api_key
-        #
-        # resource = payload.get("payload", {}).get("resource", {})
-        # if not (dataset_id := resource.get("defaultDatasetId") or payload.get("dataset_id", "")):
-        #     msg = "No Dataset ID provided. It should be provided either in payload or in actor_input"
-        #     await Actor.fail(status_message=msg)
-        #
-        # Actor.log.debug("Load Dataset ID %s and extract fields %s", dataset_id, actor_input.fields)
-        #
-        # embeddings = OpenAIEmbeddings()
-        #
-        # meta_values = actor_input.metadata_values or {}
-        # meta_fields = actor_input.metadata_fields or {}
-        #
-        # # Function from Honza Turon to load dataset.
-        # # Do we really want to create a new chunk for every field?
-        # for field in actor_input.fields:
-        #     loader = ApifyDatasetLoader(
-        #         dataset_id,
-        #         dataset_mapping_function=lambda dataset_item: Document(
-        #             page_content=get_nested_value(dataset_item, field),
-        #             metadata={
-        #                 **meta_values,
-        #                 **{key: get_nested_value(dataset_item, value) for key, value in meta_fields.items()},
-        #             },
-        #         ),
-        #     )
-        #
-        #     try:
-        #         documents = loader.load()
-        #         Actor.log.debug("Document loaded")
-        #     except Exception as e:
-        #         await Actor.fail(status_message=f"Failed to load documents for field {field}: {e}")
-        #
-        #     if actor_input.perform_chunking:
-        #         text_splitter = RecursiveCharacterTextSplitter(
-        #             chunk_size=actor_input.chunk_size, chunk_overlap=actor_input.chunk_overlap
-        #         )
-        #         documents = text_splitter.split_documents(documents)
-        #         Actor.log.debug("Documents chunked to %s chunks", len(documents))
-        #
-        #     try:
-        #         pf_from_documents = await get_database(actor_input)
-        #         pf_from_documents(documents=documents, embedding=embeddings)
-        #         Actor.log.debug("Documents inserted into database successfully")
-        #     except Exception as e:
-        #         msg = f"Document insertion failed: {str(e)}"
-        #         await Actor.set_status_message(msg)
-        #         await Actor.fail()
+        if not (arg := os.getenv("ACTOR_PATH_IN_DOCKER_CONTEXT")):
+            if Actor.is_at_home():
+                await Actor.exit(
+                    exit_code=100,
+                    status_message="This Actor was built incorrectly; no environment variable specifies which Actor "
+                    "to start. If you encounter this issue, please contact the Actor developer.",
+                )
+
+            arg = f"actors/{SupportedVectorStoresEn.pinecone.value}"
+            Actor.log.warning(
+                f"The environment variable ACTOR_PATH_IN_DOCKER_CONTEXT was not specified. "
+                f"Using default for local development: actors/{arg}"
+            )
+
+    actor_type = arg.split("/")[-1]
+    Actor.log.debug("Received start argument: %s", actor_type)
+
+    if actor_type == SupportedVectorStoresEn.chroma.value:
+        return main_f(ChromaIntegration(**actor_input), actor_input)
+    elif actor_type == SupportedVectorStoresEn.pinecone.value:
+        return main_f(PineconeIntegration(**actor_input), actor_input)
+    else:
+        await Actor.exit(
+            exit_code=10,
+            status_message=f"This Actor was built incorrectly; an unknown Actor was selected to start ({actor_type}). "
+            f"If you encounter this issue, please contact the Actor developer.",
+        )
