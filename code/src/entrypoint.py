5de428785de7847381ede1c7306406f74e00b5f0
Jiri Spilka <jirka.spilka@gmail.com>
2024-05-14 10:31:19 +0200

    Debug

--- code/src/entrypoint.py
+++ code/src/entrypoint.py
@@ -2,10 +2,10 @@ import os
 
 from apify import Actor
 
-from .constants import SupportedVectorStoresEn
-from .main import main as main_f
-from .models.chroma_input_model import ChromaIntegration
-from .models.pinecone_input_model import PineconeIntegration
+from .constants import SupportedVectorStoresEn  # type: ignore
+from .main import main as main_f  # type: ignore
+from .models.chroma_input_model import ChromaIntegration  # type: ignore
+from .models.pinecone_input_model import PineconeIntegration  # type: ignore
 
 
 async def main():
@@ -16,44 +16,30 @@ async def main():
         if not (actor_input := await Actor.get_input() or {}):
             await Actor.fail(status_message="No input provided", exit_code=1)
 
-        Actor.log.info("Received input")
-
-        Actor.log.info(
-            "Checking for the environment variable ACTOR_PATH_IN_DOCKER_CONTEXT: %s",
-            os.getenv("ACTOR_PATH_IN_DOCKER_CONTEXT"),
-        )
-        arg = os.getenv("ACTOR_PATH_IN_DOCKER_CONTEXT")
-
-        Actor.log.info("arg: %s", arg)
-
-        if not arg:
-            Actor.log.info("No environment variable ACTOR_PATH_IN_DOCKER_CONTEXT found")
+        if not (arg := os.getenv("ACTOR_PATH_IN_DOCKER_CONTEXT")):
             if Actor.is_at_home():
-                Actor.log.info("Running in local development mode")
                 await Actor.exit(
                     exit_code=100,
                     status_message="This Actor was built incorrectly; no environment variable specifies which Actor "
                     "to start. If you encounter this issue, please contact the Actor developer.",
                 )
 
-            Actor.log.info("Running in Apify cloud")
             arg = f"actors/{SupportedVectorStoresEn.pinecone.value}"
             Actor.log.warning(
                 f"The environment variable ACTOR_PATH_IN_DOCKER_CONTEXT was not specified. "
                 f"Using default for local development: actors/{arg}"
             )
 
-        Actor.log.info("Actor path: %s", arg)
-        actor_type = arg.split("/")[-1]
-        Actor.log.info("Received start argument: %s", actor_type)
+    actor_type = arg.split("/")[-1]
+    Actor.log.info("Received start argument: %s", actor_type)
 
-        if actor_type == SupportedVectorStoresEn.chroma.value:
-            return await main_f(ChromaIntegration(**actor_input), actor_input)
-        elif actor_type == SupportedVectorStoresEn.pinecone.value:
-            return await main_f(PineconeIntegration(**actor_input), actor_input)
-        else:
-            await Actor.exit(
-                exit_code=10,
-                status_message=f"This Actor was built incorrectly; an unknown Actor was selected to start ({actor_type}). "
-                f"If you encounter this issue, please contact the Actor developer.",
-            )
+    if actor_type == SupportedVectorStoresEn.chroma.value:
+        return await main_f(ChromaIntegration(**actor_input), actor_input)
+    elif actor_type == SupportedVectorStoresEn.pinecone.value:
+        return await main_f(PineconeIntegration(**actor_input), actor_input)
+    else:
+        await Actor.exit(
+            exit_code=10,
+            status_message=f"This Actor was built incorrectly; an unknown Actor was selected to start ({actor_type}). "
+            f"If you encounter this issue, please contact the Actor developer.",
+        )
