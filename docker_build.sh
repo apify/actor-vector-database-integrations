d87b9271fede5012512f87725174d7a5b5aedbd3
Jiri Spilka <jirka.spilka@gmail.com>
2024-05-14 09:29:46 +0200

    fix

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Building the Docker image"
-docker build --no-cache --tag vs_db --file shared/Dockerfile --build-arg ACTOR_PATH_IN_DOCKER_CONTEXT=pinecone .
+docker build --no-cache --tag vs_db --file shared/Dockerfile --build-arg ACTOR_PATH_IN_DOCKER_CONTEXT=actors/pinecone .
