cbe9d3b43d48ced67deb1b165d45142bb342253d
Jiri Spilka <jirka.spilka@gmail.com>
2024-10-04 14:21:55 +0200

    Increase backoff time for delete

--- code/src/constants.py
+++ code/src/constants.py
@@ -3,7 +3,8 @@ import enum
 VCR_HEADERS_EXCLUDE = ["Authorization", "Api-Key"]
 
 DAY_IN_SECONDS = 24 * 3600
-BACKOFF_MAXTIME_SECONDS = 120
+BACKOFF_MAX_TIME_SECONDS = 120
+BACKOFF_MAX_TIME_DELETE_SECONDS = 300  # 5 minutes (if many objects were added it takes time to search in the database)
 
 
 class SupportedVectorStores(str, enum.Enum):
